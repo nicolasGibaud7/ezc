8a42a878568a2525b733a380f16216b0938f2c01
nicolasGibaud7 <nicolas.gibaud7@gmail.com>
2022-09-21 16:56:31 +0200

    Add last unitary tests for ExcelFactory

--- tests/test_excel_factory/data/data_test_excel_factory.py
+++ tests/test_excel_factory/data/data_test_excel_factory.py
@@ -7,3 +7,25 @@ data_add_ingredient = [
     (["Poulet", "viande", "1", "kg", "8"], 4),
     (["Yaourt", "frais", "0.5", "kg", "4"], 5),
 ]
+
+data_create_header_row = [
+    (["Nom", "Prix", "Unite"]),
+    (["NOM", "PRIX", "UNITE"]),
+]
+
+data_get_recipe_name = [
+    ("tests/test_excel_factory/excel_files/pates_pesto.xlsx", "Pates pesto")
+]
+
+data_iterate_ingredients = [
+    (
+        "tests/test_excel_factory/excel_files/pates_pesto.xlsx",
+        [
+            ["Pates", 1],
+            ["Pesto", 0.2],
+            ["Champignons", 0.4],
+            ["Courgettes", 4],
+            ["Gruyere", 0.2],
+        ],
+    )
+]
--- tests/test_excel_factory/test_excel_factory.py
+++ tests/test_excel_factory/test_excel_factory.py
@@ -2,11 +2,14 @@ import os
 from typing import Any, List
 
 import pytest
-from ezc.constants import EXCEL_COLUMNS
+from ezc.constants import EXCEL_COLUMNS, TITLE_CELL
 from ezc.excel_factory import ExcelFactory
 from tests.test_excel_factory.data.data_test_excel_factory import (
     data_add_element,
     data_add_ingredient,
+    data_create_header_row,
+    data_get_recipe_name,
+    data_iterate_ingredients,
 )
 
 
@@ -51,3 +54,66 @@ def test_add_ingredient(ingredient_information: List[str], row_index: int):
 
     finally:
         os.remove("test_add_ingredient.xlsx")
+
+
+def test_add_title():
+    try:
+        excel_factory = ExcelFactory("test_add_title.xlsx")
+        excel_factory.add_title()
+
+        title_cell = excel_factory.current_sheet[TITLE_CELL]
+        assert title_cell.font.bold is True
+        assert title_cell.alignment.horizontal == "center"
+        assert title_cell.alignment.vertical == "center"
+        assert title_cell.border.bottom.style == "thin"
+        assert title_cell.border.top.style == "thin"
+        assert title_cell.border.left.style == "thin"
+        assert title_cell.border.right.style == "thin"
+        assert title_cell.value == "Test_add_title"
+
+        other_cell = excel_factory.current_sheet["A1"]
+        assert other_cell.font.bold is not True
+        assert other_cell.alignment.horizontal != "center"
+        assert other_cell.alignment.vertical != "center"
+        assert other_cell.border.bottom.style != "thin"
+        assert other_cell.border.top.style != "thin"
+        assert other_cell.border.left.style != "thin"
+        assert other_cell.border.right.style != "thin"
+
+    finally:
+        os.remove("test_add_title.xlsx")
+
+
+@pytest.mark.parametrize("categories", data_create_header_row)
+def test_create_header_row(categories: List[str]):
+    try:
+        excel_factory = ExcelFactory("test_create_header_row.xlsx")
+        excel_factory.create_header_row(categories)
+
+        for index, header_column in enumerate(
+            EXCEL_COLUMNS[0 : len(categories)]
+        ):
+            header_cell = excel_factory.current_sheet[f"{header_column}3"]
+            assert header_cell.style == "Headline 2"
+            assert header_cell.value == categories[index]
+
+    finally:
+        os.remove("test_create_header_row.xlsx")
+
+
+@pytest.mark.parametrize("excel_file, expected_result", data_get_recipe_name)
+def test_get_recipe_name(excel_file: str, expected_result: str):
+    excel_factory = ExcelFactory(excel_file)
+    assert excel_factory.get_recipe_name() == expected_result
+
+
+@pytest.mark.parametrize(
+    "excel_file, expected_result", data_iterate_ingredients
+)
+def test_iterate_ingredients(
+    excel_file: str, expected_result: List[List[Any]]
+):
+    excel_factory = ExcelFactory(excel_file)
+    for index, ingredient in enumerate(excel_factory.iterate_ingredients()):
+        for index2, ingredient_element in enumerate(ingredient):
+            assert ingredient_element.value == expected_result[index][index2]
