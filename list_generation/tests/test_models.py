c5ea329eb1965fae8654ef872ffb97751a297e25
NicolasGibaud7 <nicolas.gibaud7@gmail.com>
2023-01-31 15:36:25 +0100

    Refactor unit tests structure

---
+++ list_generation/tests/test_models.py
@@ -0,0 +1,116 @@
+from django.test import TestCase
+
+from list_generation.models import Category, Ingredient, Shelf, Unit
+
+
+class ShelfModelTest(TestCase):
+    def setUp(self):
+        Shelf.objects.create(name="Fruits and vegetables")
+        Shelf.objects.create(name="Dairy products")
+
+    def test_saving_shelfs(self):
+        self.assertEqual(Shelf.objects.count(), 2)
+
+    def test_retrieving_shelfs(self):
+        saved_shelfs = Shelf.objects.all()
+
+        first_saved_shelf = saved_shelfs[0]
+        second_saved_shelf = saved_shelfs[1]
+        self.assertEqual(first_saved_shelf.name, "Fruits and vegetables")
+        self.assertEqual(second_saved_shelf.name, "Dairy products")
+
+
+class UnitModelTest(TestCase):
+    def setUp(self) -> None:
+        Unit.objects.create(name="Kilogram", abbreviation="Kg")
+        Unit.objects.create(name="Gram", abbreviation="g")
+
+    def test_saving_units(self):
+        self.assertEqual(Unit.objects.count(), 2)
+
+    def test_retrieving_units(self):
+        saved_units = Unit.objects.all()
+
+        first_saved_unit = saved_units[0]
+        second_saved_unit = saved_units[1]
+        self.assertEqual(first_saved_unit.name, "Kilogram")
+        self.assertEqual(first_saved_unit.abbreviation, "Kg")
+        self.assertEqual(second_saved_unit.name, "Gram")
+        self.assertEqual(second_saved_unit.abbreviation, "g")
+
+
+class CategoryModelTest(TestCase):
+    def setUp(self) -> None:
+        Category.objects.create(name="Market")
+        Category.objects.create(name="Frozen food")
+
+    def test_saving_categories(self):
+        self.assertEqual(Category.objects.count(), 2)
+
+    def test_retrieving_categories(self):
+        saved_categories = Category.objects.all()
+
+        first_saved_category = saved_categories[0]
+        second_saved_category = saved_categories[1]
+        self.assertEqual(first_saved_category.name, "Market")
+        self.assertEqual(second_saved_category.name, "Frozen food")
+
+
+class IngredientsModelTest(TestCase):
+    def setUp(self) -> None:
+        Category.objects.create(name="Market")
+        Shelf.objects.create(name="Fruits and vegetables")
+        Unit.objects.create(name="Kilogram", abbreviation="Kg")
+        Ingredient.objects.create(
+            name="Tomate",
+            shelf=Shelf.objects.first(),
+            category=Category.objects.first(),
+            unit=Unit.objects.first(),
+            price=1.30,
+        )
+        Ingredient.objects.create(
+            name="Onion",
+            shelf=Shelf.objects.first(),
+            category=Category.objects.first(),
+            unit=Unit.objects.first(),
+        )
+
+    def test_saving_ingredients(self):
+        self.assertEqual(Ingredient.objects.count(), 2)
+
+    def test_retrieving_ingredients(self):
+        saved_ingredients = Ingredient.objects.all()
+
+        first_saved_ingredient = saved_ingredients[0]
+        second_saved_ingredient = saved_ingredients[1]
+        self.assertEqual(first_saved_ingredient.name, "Tomate")
+        self.assertEqual(second_saved_ingredient.name, "Onion")
+
+    def test_ingredients_have_shelf_attributes(self):
+        saved_ingredients = Ingredient.objects.all()
+
+        first_saved_ingredient = saved_ingredients[0]
+        self.assertEqual(
+            first_saved_ingredient.shelf.name, "Fruits and vegetables"
+        )
+
+    def test_ingredients_have_category_attributes(self):
+        saved_ingredients = Ingredient.objects.all()
+
+        first_saved_ingredient = saved_ingredients[0]
+        self.assertEqual(first_saved_ingredient.category.name, "Market")
+
+    def test_ingredient_have_unit_attributes(self):
+        saved_ingredients = Ingredient.objects.all()
+
+        first_saved_ingredient = saved_ingredients[0]
+        self.assertEqual(first_saved_ingredient.unit.name, "Kilogram")
+
+    def test_ingredient_have_price_attributes(self):
+        saved_ingredients = Ingredient.objects.all()
+
+        first_saved_ingredient = saved_ingredients[0]
+        self.assertEqual(str(first_saved_ingredient.price), "1.30")
+
+        second_saved_ingredient = saved_ingredients[1]
+        self.assertIsNone(second_saved_ingredient.price)
--- list_generation/tests/test_views.py
+++ list_generation/tests/test_views.py
@@ -10,7 +10,6 @@ from list_generation.views import (
 )
 
 
-# Create your tests here.
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
@@ -32,9 +31,6 @@ class RecipesPageTest(TestCase):
 
         self.assertTemplateUsed(response, "recipes.html")
 
-    def test_recipes_list_appears(self):
-        pass
-
 
 class IngredientsPageTest(TestCase):
     def setUp(self):
@@ -51,7 +47,7 @@ class IngredientsPageTest(TestCase):
 
         self.assertTemplateUsed(response, "ingredients.html")
 
-    def test_ingredients_are_displayed(self):
+    def test_display_all_ingredients(self):
         ingredients_info = [
             {
                 "name": "Tomate",
@@ -199,116 +195,3 @@ class IngredientDetailPageTest(TestCase):
         self.assertInHTML(
             "<title>Ingredients</title>", response.content.decode()
         )
-
-
-class ShelfModelTest(TestCase):
-    def setUp(self):
-        Shelf.objects.create(name="Fruits and vegetables")
-        Shelf.objects.create(name="Dairy products")
-
-    def test_saving_shelfs(self):
-        self.assertEqual(Shelf.objects.count(), 2)
-
-    def test_retrieving_shelfs(self):
-        saved_shelfs = Shelf.objects.all()
-
-        first_saved_shelf = saved_shelfs[0]
-        second_saved_shelf = saved_shelfs[1]
-        self.assertEqual(first_saved_shelf.name, "Fruits and vegetables")
-        self.assertEqual(second_saved_shelf.name, "Dairy products")
-
-
-class UnitModelTest(TestCase):
-    def setUp(self) -> None:
-        Unit.objects.create(name="Kilogram", abbreviation="Kg")
-        Unit.objects.create(name="Gram", abbreviation="g")
-
-    def test_saving_units(self):
-        self.assertEqual(Unit.objects.count(), 2)
-
-    def test_retrieving_units(self):
-        saved_units = Unit.objects.all()
-
-        first_saved_unit = saved_units[0]
-        second_saved_unit = saved_units[1]
-        self.assertEqual(first_saved_unit.name, "Kilogram")
-        self.assertEqual(first_saved_unit.abbreviation, "Kg")
-        self.assertEqual(second_saved_unit.name, "Gram")
-        self.assertEqual(second_saved_unit.abbreviation, "g")
-
-
-class CategoryModelTest(TestCase):
-    def setUp(self) -> None:
-        Category.objects.create(name="Market")
-        Category.objects.create(name="Frozen food")
-
-    def test_saving_categories(self):
-        self.assertEqual(Category.objects.count(), 2)
-
-    def test_retrieving_categories(self):
-        saved_categories = Category.objects.all()
-
-        first_saved_category = saved_categories[0]
-        second_saved_category = saved_categories[1]
-        self.assertEqual(first_saved_category.name, "Market")
-        self.assertEqual(second_saved_category.name, "Frozen food")
-
-
-class IngredientsModelTest(TestCase):
-    def setUp(self) -> None:
-        Category.objects.create(name="Market")
-        Shelf.objects.create(name="Fruits and vegetables")
-        Unit.objects.create(name="Kilogram", abbreviation="Kg")
-        Ingredient.objects.create(
-            name="Tomate",
-            shelf=Shelf.objects.first(),
-            category=Category.objects.first(),
-            unit=Unit.objects.first(),
-            price=1.30,
-        )
-        Ingredient.objects.create(
-            name="Onion",
-            shelf=Shelf.objects.first(),
-            category=Category.objects.first(),
-            unit=Unit.objects.first(),
-        )
-
-    def test_saving_ingredients(self):
-        self.assertEqual(Ingredient.objects.count(), 2)
-
-    def test_retrieving_ingredients(self):
-        saved_ingredients = Ingredient.objects.all()
-
-        first_saved_ingredient = saved_ingredients[0]
-        second_saved_ingredient = saved_ingredients[1]
-        self.assertEqual(first_saved_ingredient.name, "Tomate")
-        self.assertEqual(second_saved_ingredient.name, "Onion")
-
-    def test_ingredients_have_shelf_attributes(self):
-        saved_ingredients = Ingredient.objects.all()
-
-        first_saved_ingredient = saved_ingredients[0]
-        self.assertEqual(
-            first_saved_ingredient.shelf.name, "Fruits and vegetables"
-        )
-
-    def test_ingredients_have_category_attributes(self):
-        saved_ingredients = Ingredient.objects.all()
-
-        first_saved_ingredient = saved_ingredients[0]
-        self.assertEqual(first_saved_ingredient.category.name, "Market")
-
-    def test_ingredient_have_unit_attributes(self):
-        saved_ingredients = Ingredient.objects.all()
-
-        first_saved_ingredient = saved_ingredients[0]
-        self.assertEqual(first_saved_ingredient.unit.name, "Kilogram")
-
-    def test_ingredient_have_price_attributes(self):
-        saved_ingredients = Ingredient.objects.all()
-
-        first_saved_ingredient = saved_ingredients[0]
-        self.assertEqual(str(first_saved_ingredient.price), "1.30")
-
-        second_saved_ingredient = saved_ingredients[1]
-        self.assertIsNone(second_saved_ingredient.price)
