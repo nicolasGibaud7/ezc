5cd83b8df433a40e9189e312bc0d809fb0e3d8dc
NicolasGibaud7 <nicolas.gibaud7@gmail.com>
2023-02-03 16:36:17 +0100

    Refactor a bit uniit tests

--- list_generation/tests/test_views.py
+++ list_generation/tests/test_views.py
@@ -22,19 +22,9 @@ from list_generation.views import (
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
-
         self.assertTemplateUsed(response, "home.html")
-
-    def test_contains_selected_recipes_table(self):
-        response = self.client.get("/").content.decode()
-
-        self.assertIn('<table id="id_selected_recipes_table">', response)
 
     def test_get_shopping_list_element_from_context(self):
         ShoppingList.objects.create()
@@ -64,11 +54,7 @@ class HomePageTest(TestCase):
 
 
 class RecipesPageTest(TestCase):
-    def test_recipes_url_resolves_to_recipes_page_view(self):
-        found = resolve("/recipes/")
-        self.assertEqual(found.func, recipes_page)
-
-    def test_recipes_page_returns_correct_html(self):
+    def test_use_recipe_template(self):
         response = self.client.get("/recipes/")
 
         self.assertTemplateUsed(response, "recipes.html")
@@ -114,9 +100,6 @@ class RecipesPageTest(TestCase):
         self.assertIn("Ingredients count: 1", response)
 
     def test_recipe_detail_button_presence(self):
-        """
-        Check recipe detail button presence.
-        """
         recipes = ["Tomato soup", "Onion soup"]
 
         for recipe in recipes:
@@ -157,17 +140,12 @@ class RecipeDetailsPageTest(TestCase):
         )
         Recipe.objects.create(name="Tomato soup")
 
-    def test_recipe_details_url_resolves_to_recipe_details_page_view(
-        self,
-    ):
-        found = resolve(f"/recipes/{Recipe.objects.first().id}/")
-        self.assertEqual(found.func, recipe_details_page)
-
-    def test_recipe_details_page_returns_correct_html(self):
-        response = self.client.get(f"/recipes/{Recipe.objects.first().id}/")
+    def test_uses_recipe_detail_templates(self):
+        recipe_ = Recipe.objects.first()
+        response = self.client.get(f"/recipes/{recipe_.id}/")
         self.assertTemplateUsed(response, "recipe_details.html")
 
-    def test_recipe_details_page_returns_404_if_recipe_does_not_exist(self):
+    def test_returns_404_if_recipe_does_not_exist(self):
         response = self.client.get("/recipes/999/")
         self.assertEqual(response.status_code, 404)
 
@@ -205,13 +183,8 @@ class IngredientsPageTest(TestCase):
         Category.objects.create(name="Market")
         Unit.objects.create(name="Kilogram", abbreviation="Kg")
 
-    def test_ingredients_url_resolves_to_ingredients_page_view(self):
-        found = resolve("/ingredients/")
-        self.assertEqual(found.func, ingredients_page)
-
-    def test_ingredients_page_returns_correct_html(self):
+    def test_uses_ingredients_tempplates(self):
         response = self.client.get("/ingredients/")
-
         self.assertTemplateUsed(response, "ingredients.html")
 
     def test_display_all_ingredients(self):
@@ -262,24 +235,6 @@ class IngredientsPageTest(TestCase):
             self.assertIn(ingredient_info["unit"].name, response)
             self.assertIn(str(ingredient_info["price"]), response)
 
-    def test_ingredient_button_presence(self):
-        """
-        Check ingredient detail button presence.
-        """
-        Ingredient.objects.create(
-            name="Tomate",
-            shelf=Shelf.objects.first(),
-            category=Category.objects.first(),
-            unit=Unit.objects.first(),
-            price=1.3,
-        )
-        response = self.client.get("/ingredients/").content.decode()
-
-        self.assertIn(
-            f'<button id="id_details_button_{Ingredient.objects.first().id}">',
-            response,
-        )
-
     def test_ingredient_button_redirect_to_ingredient_details_page(self):
         """
         Check if the link associated to the ingredient detail button is correct
@@ -312,40 +267,18 @@ class IngredientDetailPageTest(TestCase):
             price=1.3,
         )
 
-    def test_ingredients_detail_url_resolves_to_ingredients_detail_page_view(
-        self,
-    ):
-        found = resolve(f"/ingredients/{Ingredient.objects.first().id}/")
-        self.assertEqual(found.func, ingredient_details_page)
-
-    def test_ingredients_detail_page_returns_correct_html(self):
+    def test_uses_ingredient_detail_template(self):
         response = self.client.get(
             f"/ingredients/{Ingredient.objects.first().id}/"
         )
-
         self.assertTemplateUsed(response, "ingredient_details.html")
 
     def test_retrieving_ingredient_object(self):
-        """
-        Get the ingredient object used by the view to display the ingredient.
-        We could get the information by using context["ingredient"]
-        """
         ingredient = Ingredient.objects.first()
         response = self.client.get(f"/ingredients/{ingredient.id}/").context[
             "ingredient"
         ]
         self.assertEqual(response, ingredient)
-
-    def test_retrieving_ingredient_information(self):
-        ingredient = Ingredient.objects.first()
-        response = self.client.get(
-            f"/ingredients/{ingredient.id}/"
-        ).content.decode()
-        self.assertIn(ingredient.name, response)
-        self.assertIn(ingredient.shelf.name, response)
-        self.assertIn(ingredient.category.name, response)
-        self.assertIn(ingredient.unit.name, response)
-        self.assertIn(str(ingredient.price), response)
 
     def test_unit_special_display(self):
         ingredient = Ingredient.objects.first()
@@ -357,7 +290,9 @@ class IngredientDetailPageTest(TestCase):
             response,
         )
 
-    def test_access_to_an_unknown_ingredient_details(self):
+    def test_return_404_when_trying_to_access_to_an_unknown_ingredient_details(
+        self,
+    ):
         response = self.client.get("/ingredients/999/")
         self.assertRedirects(response, "/ingredients/")
 
